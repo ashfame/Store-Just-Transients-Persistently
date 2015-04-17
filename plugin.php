da981a639b533d13b2528f048c74aab3b34fe486
Ashfame <mail@ashfame.com>
2015-04-17 15:10:33 +0530

    some more changes for public release

--- plugin.php
+++ plugin.php
@@ -3,10 +3,10 @@
 /*
 Plugin Name: Store Just Transients Persistently
 Plugin URI: https://github.com/ashfame/Store-Just-Transients-Persistently/
-Description: Store Just Transients Persistently in memcached, activate it as a regular plugin. Improves transient read speed!
+Description: Store Just Transients Persistently in memcached, when object cache can't be used. Activate it as a regular plugin. Improves transient read speed!
 Author: Ashfame
 Version: 0.1
-Author URI: http://ashfame.com
+Author URI: http://ashfame.com/
 */
 
 class Ashfame_Store_Just_Transients_Persistently {
---
+++ readme.md
@@ -0,0 +1,6 @@
+Plugin Name: Store Just Transients Persistently
+Plugin URI: https://github.com/ashfame/Store-Just-Transients-Persistently/
+Description: Store Just Transients Persistently in memcached, when object cache can't be used. Activate it as a regular plugin. Improves transient read speed!
+Author: Ashfame
+Version: 0.1
+Author URI: http://ashfame.com/
